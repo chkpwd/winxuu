4dbc1c108dc28e2ecf703c13f48d15c60fca4e5e
unix-chkpwd <unix.chkpwd@gmail.com>
2023-06-04 17:15:57 -0400

    fix(winxuu): fixed FS for templates folder

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"embed"
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -13,40 +12,19 @@ var folder embed.FS //embeds files to a virtual filesystem inside the go binary
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// w.Write([]byte(fmt.Sprintf("%v\n", r.RemoteAddr)))
-
-		// fmt.Println(r.RequestURI)
-		fmt.Println(r.URL.Path)
-
-		if r.URL.Path == "/" {
-			fldr, err := folder.ReadFile("templates/index.html")
-
-			// Handle the error for the index.html file
+		if r.URL.Path != "/" {
+			fldr, err := folder.ReadFile("templates/404.html")
 			if err != nil {
 				log.Fatal(err)
 			}
 
-			// Write the contents of the index.html file to the response
+			// Write the 404 status page
+			w.WriteHeader(http.StatusNotFound)
 			w.Write(fldr)
 			return
 		}
 
-		fldr, err := folder.ReadFile("templates/404.html")
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// Write the 404 status page
-		w.Write(fldr)
-
-	})
-
-	http.HandleFunc("/css/styles.css", func(w http.ResponseWriter, r *http.Request) {
-
-		fmt.Println(r.URL.Path)
-
-		fldr, err := folder.ReadFile("templates/styles.css")
+		fldr, err := folder.ReadFile("templates/index.html")
 
 		// Handle the error for the index.html file
 		if err != nil {
@@ -54,11 +32,13 @@ func main() {
 		}
 
 		// Write the contents of the index.html file to the response
-		w.Header().Set("Content-Type", "text/css")
 		w.Write(fldr)
 	})
 
-	err := http.ListenAndServe("0.0.0.0:8080", nil)
+	fs := http.FileServer(http.FS(folder))
+	http.Handle("/templates/", fs)
+
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
